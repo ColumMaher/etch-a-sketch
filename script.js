7ca5e5864aac6a2a7312bc1c6561f09d5d212a6b
ColumMaher <55181702+ColumMaher@users.noreply.github.com>
2023-08-29 17:33:22 +0100

    Grid generation, grid square color changing

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+const MIN_GRID_SIZE = 16
+var color = "#000000";
+
 const colorPicker = document.getElementById('colorPicker');
+const grid = document.getElementById('grid');
+const gridSizeSlider = document.getElementById('gridSize');
+const gridSizeValue = document.getElementById('sliderVal');
 
 colorPicker.addEventListener('input', function() {
-    const color = this.value;
+    color = this.value;
     console.log(color)
 })
-
-const gridSizeSlider = document.getElementById('gridSize')
-const gridSizeValue = document.getElementById('sliderVal')
 
 gridSizeSlider.addEventListener('input', function(){
     console.log(gridSizeSlider.value)
     gridSizeValue.textContent = gridSizeSlider.value + " x " + gridSizeSlider.value
 })
+
+function changeSquareColor(gridSquare){
+    gridSquare.style.backgroundColor = `${color}`;
+}
+
+function drawGrid(gridSize){
+
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for(i = 0; i < gridSize * gridSize; i++){
+        const gridSquare = document.createElement('div')
+        gridSquare.classList.add('gridSquare')
+        gridSquare.addEventListener('mouseenter', function () {
+            changeSquareColor(this)
+        })
+        grid.appendChild(gridSquare)
+    }
+}
+
+drawGrid(MIN_GRID_SIZE)

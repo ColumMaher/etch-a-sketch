35b4701fcccc5c092b290c5ad7c0bb2afb94dd0a
ColumMaher <55181702+ColumMaher@users.noreply.github.com>
2023-08-29 18:39:45 +0100

    Added rainbow mode, eraser mode, selecting between modes.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const MIN_GRID_SIZE = 16
+const MIN_GRID_SIZE = 16;
 var color = "#000000";
+var mode = "color";
 
 const colorPicker = document.getElementById('colorPicker');
 const grid = document.getElementById('grid');
 const gridSizeSlider = document.getElementById('gridSize');
 const gridSizeValue = document.getElementById('sliderVal');
 
+const colorBtn = document.getElementById('colorBtn');
+colorBtn.classList.add('activeBtn')
+const rainbowBtn = document.getElementById('rainbowBtn');
+const eraserBtn = document.getElementById('eraserBtn');
 const clearBtn = document.getElementById('clearBtn');
 const toggleBtn = document.getElementById('toggleBtn');
 
@@ -14,14 +19,16 @@ colorPicker.addEventListener('input', function() {
     console.log(color)
 })
 
-gridSizeSlider.addEventListener('input', function(){
-    console.log(gridSizeSlider.value)
-    gridSizeValue.textContent = gridSizeSlider.value + " x " + gridSizeSlider.value
+colorBtn.addEventListener('click', function(){
+    changeActiveButton("color");
 })
 
-gridSizeSlider.addEventListener('mouseup', function(){
-    clearGrid()
-    drawGrid(gridSizeSlider.value)
+rainbowBtn.addEventListener('click', function(){
+    changeActiveButton("rainbow");
+})
+
+eraserBtn.addEventListener('click', function(){
+    changeActiveButton("eraser");
 })
 
 clearBtn.addEventListener('click', function(){
@@ -34,10 +41,57 @@ toggleBtn.addEventListener('click', function(){
     toggleSquareBorder()
 })
 
-function changeSquareColor(gridSquare){
-    gridSquare.style.backgroundColor = `${color}`;
+gridSizeSlider.addEventListener('input', function(){
+    console.log(gridSizeSlider.value)
+    gridSizeValue.textContent = gridSizeSlider.value + " x " + gridSizeSlider.value
+})
+
+gridSizeSlider.addEventListener('mouseup', function(){
+    clearGrid()
+    drawGrid(gridSizeSlider.value)
+})
+
+function changeActiveButton(button){
+    if(button === "color"){
+        colorBtn.classList.add('activeBtn');
+        mode = "color";
+        rainbowBtn.classList.remove('activeBtn');
+        eraserBtn.classList.remove('activeBtn');
+    } 
+    else if(button === "rainbow"){
+        rainbowBtn.classList.add('activeBtn');
+        mode = "rainbow";
+        colorBtn.classList.remove('activeBtn');
+        eraserBtn.classList.remove('activeBtn');
+    } else {
+        eraserBtn.classList.add('activeBtn');
+        mode = "eraser";
+        colorBtn.classList.remove('activeBtn');
+        rainbowBtn.classList.remove('activeBtn');
+    }
 }
 
+function getRainbowColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeSquareColor(gridSquare){
+    if(mode === "color"){
+        gridSquare.style.backgroundColor = `${color}`;
+    } 
+    else if ( mode === "rainbow"){
+        let color = getRainbowColor();
+        gridSquare.style.backgroundColor = `${color}`;
+    }
+    else {
+        gridSquare.style.backgroundColor = "#ffffff"
+    }
+}
 
 function drawGrid(gridSize){
 
